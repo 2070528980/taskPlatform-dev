9f7c6a627a7655289d3c19720452cfd8aa44f828
wto <2070528980@qq.com>
2021-04-16 17:06:39 +0800

    0416

--- src/main/java/com/example/task/controller/FileController.java
+++ src/main/java/com/example/task/controller/FileController.java
@@ -3,11 +3,13 @@ package com.example.task.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.example.task.common.enums.CodeStatus;
+import com.example.task.common.enums.PathFormat;
 import com.example.task.common.msg.MessageResult;
 import com.example.task.common.utils.DrmFileUtils;
 import com.example.task.common.utils.MD5Utils;
 import com.example.task.common.utils.MinioUtil;
 import com.example.task.service.ITUserService;
+import net.coobird.thumbnailator.Thumbnails;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
@@ -17,9 +19,8 @@ import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 
@@ -37,43 +38,84 @@ public class FileController {
 
     private Logger logger = LoggerFactory.getLogger(FileController.class);
 
+    @Value("${drmPath}")
+    private String drmPath;
 
 
+//    @RequestMapping("/uploadApi2")
+//    public Object uploadApi2(
+//            HttpServletRequest request) throws IOException {
+//
+//        System.out.println("------------uploadApi-----------");
+//
+//        try {
+//            StandardMultipartHttpServletRequest mureq = (StandardMultipartHttpServletRequest) request;
+//            MultipartFile app = mureq.getFile("file");
+//            if(app == null){
+//                return MessageResult.error("上传文件为空");
+//            }
+//            String objectName = UUID.randomUUID().toString();
+//            String originalFilename = app.getOriginalFilename(); // 原名称
+//            String suffix = DrmFileUtils.getSuffix(originalFilename);
+//            String bucketName = "task";
+//            objectName = objectName + '.' + suffix;
+//
+//            // 上传文件
+//            MinioUtil.uploadFile_InputStream(objectName, app.getInputStream(),
+//                    app.getSize(), app.getContentType(),bucketName);
+//
+//            JSONObject result = new JSONObject();
+//            result.put("code", 0);
+//            result.put("msg", "success");
+//            JSONObject data = new JSONObject();
+//            data.put("src",  bucketName + File.separator + objectName);
+//            data.put("url",  bucketName + File.separator + objectName);
+//            data.put("title", originalFilename);
+//            result.put("data", data);
+//
+//            return result;
+//        } catch (Exception e) {
+//            return MessageResult.error(e.getMessage());
+//        }
+//    }
+
+
+    @ResponseBody
     @RequestMapping("/uploadApi")
-    public Object uploadApi(
-            HttpServletRequest request) throws IOException {
+    public Object character_uploadFile(@RequestPart(name = "edit", required = false) MultipartFile file) {
 
-        System.out.println("------------uploadApi-----------");
 
-        try {
-            StandardMultipartHttpServletRequest mureq = (StandardMultipartHttpServletRequest) request;
-            MultipartFile app = mureq.getFile("file");
-            if(app == null){
-                return MessageResult.error("上传文件为空");
+//        System.out.println("------------uploadApi-----------");
+        String originalFilename = file.getOriginalFilename(); // 原名称
+        String suffix = DrmFileUtils.getSuffix(originalFilename); //获取文件后缀名
+        String format = "/images/{yyyy}{mm}{dd}/{time}{rand:6}";
+        String fileName = PathFormat.parse(format) + '.' + suffix;
+//        上传图片返回地址
+        MinioUtil.uploadFile(file, fileName, "task");
+//        判断是否图片且大小是否超过1024kb
+        if (DrmFileUtils.isImage(DrmFileUtils.MultipartFileToFile(file)) && file.getSize() / 1024 > 1024) {
+            try {
+                //              压缩图片
+//                of("原图文件的路径")
+//              scale 1f就是原图长宽，0.5就是原图的一半长宽
+//              outputQuality是图片的质量 越接近于1质量越好，越接近于0质量越差。
+//                toFile("压缩后文件的路径");
+                Thumbnails.of(drmPath + "/task" + fileName).scale(1f).outputQuality(0.25f).toFile(drmPath + "/task" + fileName);
+                logger.info("压缩成功");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            String objectName = UUID.randomUUID().toString();
-            String originalFilename = app.getOriginalFilename(); // 原名称
-            String suffix = DrmFileUtils.getSuffix(originalFilename);
-            String bucketName = "task";
-            objectName = objectName + '.' + suffix;
 
-            // 上传文件
-            MinioUtil.uploadFile_InputStream(objectName, app.getInputStream(),
-                    app.getSize(), app.getContentType(),bucketName);
-
-            JSONObject result = new JSONObject();
-            result.put("code", 0);
-            result.put("msg", "success");
-            JSONObject data = new JSONObject();
-            data.put("src",  bucketName + File.separator + objectName);
-            data.put("url",  bucketName + File.separator + objectName);
-            data.put("title", originalFilename);
-            result.put("data", data);
-
-            return result;
-        } catch (Exception e) {
-            return MessageResult.error(e.getMessage());
         }
+
+        JSONObject result = new JSONObject();
+        result.put("code", 0);
+        result.put("msg", "success");
+        JSONObject data = new JSONObject();
+        data.put("src", "/task" + fileName);
+        data.put("title", "task");
+        result.put("data", data);
+        return result;
     }
 
 
