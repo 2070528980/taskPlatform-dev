1d8a029ad1db7d23d143eeb8a897621662e56bab
wto <2070528980@qq.com>
2021-04-20 15:41:26 +0800

    0420

--- src/main/java/com/example/task/controller/H5Controller.java
+++ src/main/java/com/example/task/controller/H5Controller.java
@@ -1,7 +1,11 @@
 package com.example.task.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.example.task.common.msg.MessageResult;
+import com.example.task.entity.TTask;
+import com.example.task.service.ITLabelService;
 import com.example.task.service.ITTaskService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,14 +30,20 @@ public class H5Controller {
     @Autowired
     private ITTaskService taskService;
 
+    @Autowired
+    private ITLabelService labelService;
+
     private Logger logger = LoggerFactory.getLogger(H5Controller.class);
 
 
     @RequestMapping("/taskListAll")
     @ResponseBody
-    public Object taskListAll(String keyword) {
-
-        return MessageResult.success(taskService.list());
+    public Object taskListAll(String keyword,String select) {
+        System.out.println("keyword:"+keyword);
+        System.out.println("select:"+select);
+        QueryWrapper<TTask> queryWrapper = new QueryWrapper<TTask>().like("projectLabel",select)
+                .and(wrapper -> wrapper.like("projectName",keyword).or().like("projectLabel",keyword));
+        return MessageResult.success(taskService.list(queryWrapper));
     }
 
 
@@ -42,6 +52,13 @@ public class H5Controller {
     public Object taskByID(Integer id) {
 
         return MessageResult.success(taskService.getById(id));
+    }
+
+    @RequestMapping("/labelListAll")
+    @ResponseBody
+    public Object labelListAll(String keyword) {
+
+        return MessageResult.success(labelService.list());
     }
 
 
