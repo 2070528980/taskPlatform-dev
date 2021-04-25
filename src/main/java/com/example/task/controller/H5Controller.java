cd37028452fd1e353118ef02a6f024c9f88942c0
wto <2070528980@qq.com>
2021-04-25 15:08:27 +0800

    0425

--- src/main/java/com/example/task/controller/H5Controller.java
+++ src/main/java/com/example/task/controller/H5Controller.java
@@ -1,8 +1,11 @@
 package com.example.task.controller;
 
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.task.common.msg.MessageResult;
 import com.example.task.entity.TTask;
 import com.example.task.service.ITLabelService;
@@ -11,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,12 +42,20 @@ public class H5Controller {
 
     @RequestMapping("/taskListAll")
     @ResponseBody
-    public Object taskListAll(String keyword,String select) {
-//        System.out.println("keyword:"+keyword);
-//        System.out.println("select:"+select);
+    public Object taskListAll(@RequestParam(value="keyword",required = false,defaultValue = "") String keyword
+            ,@RequestParam(value="select",required = false,defaultValue = "") String select
+            , @RequestParam(value="pageNum",required = false,defaultValue = "1") Integer pageNum
+            , @RequestParam(value="pageSize",required = false,defaultValue = "8") Integer pageSize) {
+        System.out.println("keyword:"+keyword);
+        System.out.println("select:"+select);
+        System.out.println("pageNum:"+pageNum);
+        System.out.println("pageSize:"+pageSize);
+        IPage<TTask> page = new Page<>(pageNum,pageSize);
         QueryWrapper<TTask> queryWrapper = new QueryWrapper<TTask>().eq("projectState",1).like("projectLabel",select)
                 .and(wrapper -> wrapper.like("projectName",keyword).or().like("projectLabel",keyword)).orderByDesc("updateDate");
-        return MessageResult.success(taskService.list(queryWrapper));
+        IPage<TTask> pigpenIPage =  taskService.page(page,queryWrapper);
+//        String json = JSON.toJSONString(pigpenIPage.getRecords());
+        return MessageResult.success(pigpenIPage.getRecords());
     }
 
 
